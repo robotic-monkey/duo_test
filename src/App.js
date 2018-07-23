711f66ed14aebff82ec3271028c29568fb45b6fc
robotic-monkey <seanpatmiller@gmail.com>
2018-07-22 22:24:48 -0800

    treats

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Ninja nuggets<code>src/App.js</code> and save to reload.
+          Master Splinter<code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
